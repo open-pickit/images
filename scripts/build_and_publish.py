2a05782dbe59f593bfb3d3f7cacb215ae787d872
Di Maulo Agustin <adimaulo@pickit.net>
2023-05-25 18:06:39 -0300

    fix images name

--- scripts/build_and_publish.py
+++ scripts/build_and_publish.py
@@ -2,11 +2,11 @@ import subprocess
 
 def build_and_push_image(image_name, dockerfile_path, repository, tag):
     # Build the Docker image
-    build_command = f"docker build -t {image_name} -f {dockerfile_path} ."
+    build_command = f"docker build -t {repository}/{image_name} -f {dockerfile_path} ."
     subprocess.run(build_command, shell=True, check=True)
 
     # Push the Docker image to a repository
-    push_command = f"docker tag {repository}/{image_name}:{tag} && docker push {repository}/{image_name}:{tag}"
+    push_command = f"docker tag {repository}/{image_name} {repository}/{image_name}:{tag} && docker push {repository}/{image_name}:{tag}"
     subprocess.run(push_command, shell=True, check=True)
 
 # Example usage

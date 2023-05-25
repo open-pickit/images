21bf16f210c3049b2e1a15e4261689165fa667dc
Di Maulo Agustin <adimaulo@pickit.net>
2023-05-25 19:06:08 -0300

    added docker cache

--- scripts/build_and_publish.py
+++ scripts/build_and_publish.py
@@ -1,13 +1,19 @@
 import subprocess
 
+
 def build_and_push_image(image_name, dockerfile_path, repository, tag):
+    # Set Docker engine
+    build_command = f"docker buildx create --use --driver=docker-containe"
+    subprocess.run(build_command, shell=True, check=True)
+
     # Build the Docker image
-    build_command = f"docker build --cache-from \"type=local,src=./cache\" --cache-to \"type=local,src=./cache\" -t {repository}/{image_name} -f {dockerfile_path} ."
+    build_command = f"docker buildx build --cache-from \"type=local,src=./cache\" --cache-to \"type=local,src=./cache\" -t {repository}/{image_name} -f {dockerfile_path} ."
     subprocess.run(build_command, shell=True, check=True)
 
     # Push the Docker image to a repository
     push_command = f"docker tag {repository}/{image_name} {repository}/{image_name}:{tag} && docker push {repository}/{image_name}:{tag}"
     subprocess.run(push_command, shell=True, check=True)
+
 
 # Example usage
 image_name = "cli-aws-kubectl"

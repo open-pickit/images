ae60117277d0f80c3b1176bf0c3b0d6a0692bb0b
Di Maulo Agustin <adimaulo@pickit.net>
2023-05-29 16:42:57 -0300

    added node-build-ecr image

--- scripts/build_and_publish.py
+++ scripts/build_and_publish.py
@@ -1,10 +1,17 @@
 import subprocess
+import os
+
+images_base_dir = 'docker-images'
 
 
-def build_and_push_image(image_name, dockerfile_path, repository, tag):
-    # Set Docker engine
+def set_docker_engine():
     build_command = f"docker buildx create --use --driver=docker-container"
     subprocess.run(build_command, shell=True, check=True)
+
+
+def build_and_push_image(image_name, tag):
+    dockerfile_path = os.path.join(images_base_dir, image_name, "Dockerfile")
+    repository = "pickittechnology"
 
     # Build the Docker image
     build_command = f"docker buildx build --cache-from \"type=local,src=./cache\" --cache-to \"type=local,dest=./cache\" -t {repository}/{image_name}:{tag} -f {dockerfile_path} --push ."
@@ -12,9 +19,11 @@ def build_and_push_image(image_name, dockerfile_path, repository, tag):
 
 
 # Example usage
-image_name = "cli-aws-kubectl"
-dockerfile_path = "docker-images/cli-aws-kubectl/Dockerfile"
-repository = "pickittechnology"
 tag = "latest"
 
-build_and_push_image(image_name, dockerfile_path, repository, tag)
+set_docker_engine()
+
+images_dirs = [d for d in os.listdir(images_base_dir) if os.path.isdir(os.path.join(images_base_dir, d))]
+
+for image in images_dirs:
+    build_and_push_image(image, tag)
